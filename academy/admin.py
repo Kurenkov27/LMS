642054125a3e5f66539451ae415e9d81f3b09dc5
Nikolay <kurenkov.mykola@ukr.net>
2021-02-05 00:56:05 +0200

    Add new files

--- academy/admin.py
+++ academy/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Student
-from .models import Lecturer
-from .models import Group
+from .models import Student, Lecturer, Group
 
 # Register your models here.
 admin.site.register(Student)
