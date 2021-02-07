6c1473443b551fc5809deb53ec352273cb174855
Nikolay <kurenkov.mykola@ukr.net>
2021-02-08 00:08:48 +0200

    Homework 7

--- academy/urls.py
+++ academy/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', views.index),
     path('students', views.get_students),
     path('lecturers', views.get_lecturers),
-    path('groups', views.get_groups)
+    path('groups', views.get_groups),
+    path('students/create_student/', views.get_student),
+    path('lecturers/create_lecturer/', views.get_lecturer),
+    path('groups/create_group/', views.get_group)
 ]
--- academy/views.py
+++ academy/views.py
@@ -1,8 +1,11 @@
+from academy.forms import StudentForm, LecturerForm, GroupForm
 from academy.models import Group, Lecturer, Student
 
 from django.http import HttpResponse
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+
 # Create your views here.
 
 
@@ -24,3 +27,54 @@ def get_lecturers(request):
 def get_groups(request):
     groups = Group.objects.all().order_by('-group_id')
     return render(request, 'academy/get_groups.html', {'groups': groups})
+
+
+def get_student(request):
+    new_student = None
+
+    if request.method == 'POST':
+        student_form = StudentForm(data=request.POST)
+        if student_form.is_valid():
+            new_student = student_form.save(commit=False)
+            new_student.save()
+
+    context = {
+        'student_form': StudentForm(),
+        'new_student': new_student
+    }
+
+    return render(request, 'academy/create_student.html', context)
+
+
+def get_lecturer(request):
+    new_lecturer = None
+
+    if request.method == 'POST':
+        lecturer_form = LecturerForm(data=request.POST)
+        if lecturer_form.is_valid():
+            new_lecturer = lecturer_form.save(commit=False)
+            new_lecturer.save()
+
+    context = {
+        'lecturer_form': LecturerForm(),
+        'new_lecturer': new_lecturer
+    }
+
+    return render(request, 'academy/create_lecturer.html', context)
+
+
+def get_group(request):
+    new_group = None
+
+    if request.method == 'POST':
+        group_form = GroupForm(data=request.POST)
+        if group_form.is_valid():
+            new_group = group_form.save(commit=False)
+            new_group.save()
+
+    context = {
+        'group_form': GroupForm(),
+        'new_group': new_group
+    }
+
+    return render(request, 'academy/create_group.html', context)
