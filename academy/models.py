480509960a7068cd225dd0dd730074850c165c47
Nikolay <kurenkov.mykola@ukr.net>
2021-02-08 20:47:37 +0200

    Homework7: Final

--- academy/models.py
+++ academy/models.py
@@ -8,6 +8,9 @@ class Student(models.Model):
     last_name = models.CharField(max_length=30, blank=True, null=True)
     email = models.EmailField(max_length=250)
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 class Lecturer(models.Model):
     lecturer_id = models.AutoField(primary_key=True)
@@ -15,9 +18,15 @@ class Lecturer(models.Model):
     last_name = models.CharField(max_length=30, blank=True, null=True)
     email = models.EmailField(max_length=150)
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 class Group(models.Model):
     group_id = models.AutoField(primary_key=True)
     course = models.CharField(max_length=150)
     students = models.ManyToManyField(Student)
     teacher = models.OneToOneField(Lecturer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.course}, #{len(self.students)} students'
