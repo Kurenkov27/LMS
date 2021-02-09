c2f45c8345b590d03fe25caba5e20a027b19df95
Nikolay <kurenkov.mykola@ukr.net>
2021-02-09 12:05:10 +0200

    Travis CI fix issues

--- academy/views.py
+++ academy/views.py
@@ -3,7 +3,7 @@ from academy.models import Group, Lecturer, Student
 
 from django.http import HttpResponse
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 
 
 # Create your views here.
