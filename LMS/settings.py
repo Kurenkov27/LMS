96628797024a06dbae627a7dea32207d8b34c8fe
Nikolay <kurenkov.mykola@ukr.net>
2021-02-16 02:01:12 +0200

    HW9

--- LMS/settings.py
+++ LMS/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'academy'
+    'academy.apps.AcademyConfig',
+    'silk',
+    'logger'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'silk.middleware.SilkyMiddleware',
+    'logger.middleware.LoggerMiddleware'
 ]
 
 ROOT_URLCONF = 'LMS.urls'
--- academy/apps.py
+++ academy/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class AcademyConfig(AppConfig):
     name = 'academy'
+
+    def ready(self):
+        import academy.signals
+
