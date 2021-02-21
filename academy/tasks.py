fbfd4a09933fed2e388c5671715005894df55429
Nikolay <kurenkov.mykola@ukr.net>
2021-02-21 22:46:30 +0200

    Still doesn't work

--- academy/tasks.py
+++ academy/tasks.py
@@ -1,12 +1,9 @@
-from celery import shared_task
+from celery import shared_task, app
 
-from LMS.celery import app
+#from LMS.celery import app
 from logger.models import LogRecord
 
-shared_task
-
-
-@app.task
+@shared_task
 def clear_log():
     print('clear_log worked!')
     LogRecord.objects.delete()
